82b77c0ab0a31f41c8c73fdd7929a1d189f77434
apc <alexcurran.va@gmail.com>
2020-03-30 14:23:02 -0500

    Added jest testing

---
+++ chapter1/string-permutation.js
@@ -0,0 +1,12 @@
+// Create a function that takes in two strings and compares if they have identical character counts.
+
+function strPermutation(str1, str2) {
+    if (str1.length !== str2.length) return false;
+
+    const sortedStr1 = str1
+        .split("")
+        .sort()
+        .join("");
+
+    return sortedStr1;
+}
